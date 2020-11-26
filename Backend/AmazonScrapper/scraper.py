d4d06aa6009b8a68a12f746d7a92368d640769c7
osori-magu <minjuda34@gmail.com>
2020-11-27 00:23:49 +0900

    initial settings

---
+++ Backend/AmazonScrapper/scraper.py
@@ -0,0 +1,10 @@
+import time
+import json
+import os
+import random
+from datetime import datetime
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+from .scraper_config import (
+    
+)
